fa496a62e183f9a41f40d3d5e13d0131dd343661
Mathilde Robert <m.robert@skema.edu>
2019-12-29 21:44:11 +0100

    delete update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,10 +9,26 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @ingredient = Ingredient.new
   end
 
   def create
     @recipe = Recipe.new
     @recipe.save!
+  end
+
+  def edit
+    @recipe = Recipe.find(params[:id])
+  end
+
+  def update
+    @recipe = Recipe.find(params[:id])
+    @recipe.update(params[:recipe])
+  end
+
+  def destroy
+    @recipe = Recipe.find(params[:id])
+    @recipe.destroy
+    redirect_to recipes_path
   end
 end
