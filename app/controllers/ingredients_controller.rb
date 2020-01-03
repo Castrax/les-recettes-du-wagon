2ddb76d6ee49926ce1ad1609769d19669e41fec0
Mathilde Robert <m.robert@skema.edu>
2020-01-03 11:05:58 +0100

    ingredients

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,19 @@
 class IngredientsController < ApplicationController
+  def new
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @recipe = Recipe.find(params[:recipe_id])
+    @ingredient = Ingredient.new(ingredient_params)
+    @ingredient.save!
+    redirect_to recipe_path(@recipe)
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :month)
+  end
 end
