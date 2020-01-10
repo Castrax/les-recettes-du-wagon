0d45ba33d2c9102c1b7bf68938f053ae36d68c0d
Mathilde Robert <m.robert@skema.edu>
2020-01-10 17:31:24 +0100

    strong parameters nom prenoms surnom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:prenom, :nom, :surnom])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:prenom, :nom, :surnom])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:prenom, :nom, :surnom])
+  end
 end
