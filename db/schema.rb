20dcc4874b1d41dd28a62d80eed95c3f90b0be31
Mathilde Robert <m.robert@skema.edu>
2020-01-13 16:04:46 +0100

    ratings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_13_082240) do
+ActiveRecord::Schema.define(version: 2020_01_13_134818) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2020_01_13_082240) do
     t.time "total_time"
     t.string "kitchen_type"
     t.bigint "user_id"
+    t.integer "rating"
   end
 
   create_table "users", force: :cascade do |t|
